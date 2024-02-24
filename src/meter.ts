738bfcf3735c3c1039cb018009b13e9622b943f5
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2024-02-24 15:32:55 +0900

    add meter

---
+++ src/meter.ts
@@ -0,0 +1,25 @@
+import { metrics, type Meter } from "@opentelemetry/api";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
+import { MeterProvider, PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { type IResource } from "@opentelemetry/resources";
+
+type MeterConfig = {
+  resource: IResource;
+  url: string;
+  meterName: string;
+};
+
+export const getMeter = (config: MeterConfig): Meter => {
+  const meterProvider = new MeterProvider({
+    resource: config.resource,
+    readers: [
+      new PeriodicExportingMetricReader({
+        exporter: new OTLPMetricExporter({ url: `${config.url}/v1/metrics` }),
+        exportIntervalMillis: 1000,
+      }),
+    ],
+  });
+  metrics.setGlobalMeterProvider(meterProvider);
+
+  return metrics.getMeter(config.meterName);
+};
