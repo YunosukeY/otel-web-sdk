24c619ee4df45bac5e2e2c09c25d00801d2e0c9f
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2024-02-24 15:35:45 +0900

    add logger

---
+++ src/logger.ts
@@ -0,0 +1,20 @@
+import { logs, type Logger } from "@opentelemetry/api-logs";
+import { BatchLogRecordProcessor, LoggerProvider } from "@opentelemetry/sdk-logs";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+import { type IResource } from "@opentelemetry/resources";
+
+type LoggerConfig = {
+  resource: IResource;
+  url: string;
+  loggerName: string;
+};
+
+export const getLogger = (config: LoggerConfig): Logger => {
+  const loggerProvider = new LoggerProvider({ resource: config.resource });
+  loggerProvider.addLogRecordProcessor(
+    new BatchLogRecordProcessor(new OTLPLogExporter({ url: `${config.url}/v1/logs` })),
+  );
+  logs.setGlobalLoggerProvider(loggerProvider);
+
+  return logs.getLogger(config.loggerName);
+};
