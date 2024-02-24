291072b227c199e8610d7b5814aeb79aa48683d5
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2024-02-24 15:11:50 +0900

    add resource

---
+++ src/resource.ts
@@ -0,0 +1,13 @@
+import { Resource, detectResourcesSync, type IResource } from "@opentelemetry/resources";
+import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
+import { browserDetector } from "@opentelemetry/opentelemetry-browser-detector";
+
+export const getResource = (serviceName: string): IResource => {
+  const baseResource = new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+  });
+  const detectedResources = detectResourcesSync({
+    detectors: [browserDetector],
+  });
+  return baseResource.merge(detectedResources);
+};
