a1881ee5034caeb954f53969b9db406282f83c9f
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2024-02-24 15:27:41 +0900

    add tracer

---
+++ src/tracer.ts
@@ -0,0 +1,34 @@
+import { ZoneContextManager } from "@opentelemetry/context-zone";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { registerInstrumentations } from "@opentelemetry/instrumentation";
+import { FetchInstrumentation } from "@opentelemetry/instrumentation-fetch";
+import { BatchSpanProcessor, WebTracerProvider } from "@opentelemetry/sdk-trace-web";
+import { W3CTraceContextPropagator } from "@opentelemetry/core";
+import { trace, type Tracer } from "@opentelemetry/api";
+import { type IResource } from "@opentelemetry/resources";
+
+type TracerConfig = {
+  resource: IResource;
+  url: string;
+  tracerName: string;
+};
+
+export const getTracer = (config: TracerConfig): Tracer => {
+  const provider = new WebTracerProvider({ resource: config.resource });
+  provider.addSpanProcessor(new BatchSpanProcessor(new OTLPTraceExporter({ url: `${config.url}/v1/traces` })));
+  provider.register({
+    contextManager: new ZoneContextManager(),
+    propagator: new W3CTraceContextPropagator(),
+  });
+  trace.setGlobalTracerProvider(provider);
+
+  registerInstrumentations({
+    instrumentations: [
+      new FetchInstrumentation({
+        clearTimingResources: true,
+      }),
+    ],
+  });
+
+  return trace.getTracer(config.tracerName);
+};
