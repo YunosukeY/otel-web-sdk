1bcb34d2b0c062df9e485744e13e5e707ebe4c7d
Yunosuke Yamada <39757050+YunosukeY@users.noreply.github.com>
2024-02-25 09:19:46 +0900

    chore: update example (#37)

--- examples/nextjs/src/Examples/LogExample.tsx
+++ examples/nextjs/src/Examples/LogExample.tsx
@@ -1,13 +1,8 @@
 import type React from "react";
 import { logger } from "./otel";
-import { SeverityNumber } from "@opentelemetry/api-logs";
 
 const LogExample: React.FC = () => {
-  logger.emit({
-    severityNumber: SeverityNumber.INFO,
-    severityText: "INFO",
-    body: "client component",
-  });
+  logger.info("client component");
 
   return null;
 };
--- examples/nextjs/src/Examples/TraceExample.tsx
+++ examples/nextjs/src/Examples/TraceExample.tsx
@@ -1,13 +1,14 @@
-import { context, trace } from "@opentelemetry/api";
 import type React from "react";
 import { useEffect } from "react";
 import { tracer } from "./otel";
 
 const ClientFetchTraceExample: React.FC = () => {
   useEffect(() => {
-    const span = tracer.startSpan("client manual Span");
+    const span = tracer.startSpan("client manual span");
+    span.end();
+
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    context.with(trace.setSpan(context.active(), span), async () => {
+    tracer.startActiveSpan("nested span", async (span) => {
       await fetch("https://example.com/", {
         method: "GET",
         mode: "no-cors",
